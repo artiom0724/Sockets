b04ff8675e9d02fc6a06ca0cd1f7beccba034d9e
XiAOMi <XiAOMi@DESKTOP-MAJJ7MO>
2018-12-14 00:13:20 +0300

    hz

--- ChatSockets/Models/IpUtils.cs
+++ ChatSockets/Models/IpUtils.cs
@@ -11,20 +11,32 @@ namespace ChatSockets.Models
 {
 	public static class IpUtils
 	{
-		private static IPInterfaceProperties InterfaceProperties => NetworkInterface.GetAllNetworkInterfaces()
-			.FirstOrDefault(ni =>
-				ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 &&
-				ni.OperationalStatus == OperationalStatus.Up)?.GetIPProperties();
+		private static IPInterfaceProperties InterfaceProperties
+		{
+			get
+			{
+				var interfaceData = NetworkInterface.GetAllNetworkInterfaces();
+				foreach (var test in interfaceData)
+				{
+					if (test != null && test.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 &&
+													test.OperationalStatus == OperationalStatus.Up)
+					{
+						return test.GetIPProperties();
+					}
+				}
+				return null;
+			}
+		}
 
 		private static UnicastIPAddressInformation AddressInformation =>
 			InterfaceProperties.UnicastAddresses.FirstOrDefault(uai =>
 				uai.Address.AddressFamily == AddressFamily.InterNetwork);
 
-		public static IPAddress LocalAddress => AddressInformation.Address;
+		public static IPAddress LocalAddress => AddressInformation?.Address;
 
-		public static IPAddress SubnetMask => AddressInformation.IPv4Mask;
+		public static IPAddress SubnetMask => AddressInformation?.IPv4Mask;
 
-		public static IPAddress BroadcastAddress => LocalAddress.GetBroadcastAddress(SubnetMask);
+		public static IPAddress BroadcastAddress => LocalAddress?.GetBroadcastAddress(SubnetMask);
 
 		public static List<IPAddress> MulticastAddresses => InterfaceProperties.MulticastAddresses
 			.Where(ma => ma.Address.AddressFamily == AddressFamily.InterNetwork).Select(ma => ma.Address).ToList();
